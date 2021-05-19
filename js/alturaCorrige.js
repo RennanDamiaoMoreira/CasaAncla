84f4cf1941b49c2cad916202a14a30b135cabefe
Rennan <rennandamiao@gmail.com>
2021-05-19 14:31:05 -0300

    Correção de Altura das imagens

---
+++ js/alturaCorrige.js
@@ -0,0 +1,7 @@
+var altura= $('img#imgReferencia').height()
+console.log(altura);
+var tamanho =document.getElementsByClassName('corrige');
+console.log(tamanho);
+for(var i=0;i<tamanho.length;i++){
+    tamanho.item(i).style.height=altura+'px';
+}
